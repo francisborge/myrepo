0221b5dc56c114ec0e7c7f14db9782b357ebbf01
Francisco Borge <francisco.borge@btinternet.com>
2020-03-18 16:09:47 +0100

    Code for GitHub

--- Test.R
+++ Test.R
@@ -6,3 +6,12 @@ y = 10
 x+y
 
 sessionInfo()
+
+#install.packages("usethis")
+library(usethis)
+
+# store token in a variable
+edit_r_environ()
+
+# connect R with github
+use_github(protocol = "https", auth_token = Sys.getenv("GITHUB_PAT"))
